60ff2ff958f83eb88d0633df12ef2c8c3ae6f6f3
Linus Jäderlund <linus.jaderlund@hjart-lungfonden.se>
2020-05-14 08:49:07 +0200

    Adds ciphering

---
+++ cipher.js
@@ -0,0 +1,28 @@
+const { gzipSync } = require("zlib");
+const crypto = require("crypto");
+const fs = require("fs");
+
+module.exports = (file, secret) => {
+  const algorithm = "aes-192-cbc";
+  const key = crypto.scryptSync(secret, "salt", 24);
+  const iv = Buffer.alloc(16, 0);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const data = gzipSync(fs.readFileSync(file, "utf8"));
+
+  let encrypted = "";
+
+  cipher.on("readable", () => {
+    let chunk;
+    while (null !== (chunk = cipher.read())) {
+      encrypted += chunk.toString("hex");
+    }
+  });
+
+  cipher.on("end", () => {
+    fs.writeFileSync(file, encrypted, "hex");
+    console.log(`Your file "${file}" is now a secret`);
+  });
+
+  cipher.write(data);
+  cipher.end();
+};
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("WADDUP BILL!?!");
+const fs = require("fs");
+const cipher = require("./cipher");
+const [, , file, secret] = process.argv;
+
+const init = () => {
+  if (!fs.existsSync(file)) {
+    return console.log("File path passed to function does not exist");
+  }
+
+  if (!secret) {
+    return console.log("Must pass secret as second parameter to function");
+  }
+
+  cipher(file, secret);
+};
+
+init();
